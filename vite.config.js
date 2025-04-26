df13301c6b77ec253ef80200337293ca82ed9e5d
kumar-github1 <kumarsssdsk@gmail.com>
2025-04-26 16:31:21 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || '/weather-app'
+  // base: process.env.VITE_BASE_PATH || '/weather-app'
 })
